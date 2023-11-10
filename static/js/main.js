36c7be7109a14b6e8fbaafbc44a001ace02ef099
Akarsh <79101639+AkarshAnilkumar@users.noreply.github.com>
2023-11-10 22:06:49 +0530

    first commit

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+function submitText() {
+    const textAreaValue = document.getElementById('text-area').value;
+    // Do something with the submitted text, e.g., save to a database or perform some action.
+    alert(`You entered:\n${textAreaValue}`);
+  }
+  
